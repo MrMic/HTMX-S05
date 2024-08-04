c5f3371f39f9dd0433e3110779cf89c6e211fb2e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-04 15:08:17 +0200

    feat: Unlock Automatic & Progressive Enhancement w/ hx-boost

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
         <link rel="stylesheet" href="/main.css">
         <script src="/htmx.js" defer></script>
       </head>
-      <body>
+      <body hx-boost="true">
         <header id="main-header">
           <div id="main-title">
             <a href="/">
@@ -29,22 +29,18 @@ app.get('/', (req, res) => {
           <h2>Elegant Clothing For Everyone</h2>
 
           <ul id="products">
-            ${PRODUCTS.map(
-    (product) => `
-              <article class="product">
-                <a 
-                  hx-get="/products/${product.id}" 
-                  hx-target="body"
-                  hx-push-url="/products/${product.id}"
-                >
-                  <img src="/images/${product.image}" alt="${product.title}" />
-                  <div class="product-content">
-                    <h3>${product.title}</h3>
-                    <p class="product-price">$${product.price}</p>
-                  </div>
-                </a>
-              </article>
-            `
+            ${PRODUCTS.map((product) => `
+                        <article class="product">
+                          <a href="/products/${product.id}" 
+                          >
+                            <img src="/images/${product.image}" alt="${product.title}" />
+                            <div class="product-content">
+                              <h3>${product.title}</h3>
+                              <p class="product-price">$${product.price}</p>
+                            </div>
+                          </a>
+                        </article>
+                      `
   ).join('')}
           </ul>
         </main>
